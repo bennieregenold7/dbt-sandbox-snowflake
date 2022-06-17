8d51fbc8da308316de2bcf2cd6fd5b996ab5c08d
Bennie Regenold <bennie.regenold@dbtlabs.com>
2022-06-17 12:49:50 -0500

    add sqlfluff

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,9 +1,14 @@
 
 -- Use the `ref` function to select from other models
 
-select *
-from {{ ref('my_first_dbt_model') }}
-where id = 1
-union 
-select *
-from {{ ref('my_unrelated_model') }}
+with t1 as (
+    select *
+    from {{ ref('my_first_dbt_model') }}
+    where id = 1
+
+    union 
+
+    select *
+    from {{ ref('my_unrelated_model') }}
+)
+select * from t1

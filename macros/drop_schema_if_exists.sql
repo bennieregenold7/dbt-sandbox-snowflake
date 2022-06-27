c65d691df47483fce1d71dddee5c5fe4f360dbcc
Bennie Regenold <bennie.regenold@dbtlabs.com>
2022-06-27 15:01:21 -0500

    update schema drop macro and confirm new api key

--- macros/drop_schema_if_exists.sql
+++ macros/drop_schema_if_exists.sql
@@ -1,6 +1,6 @@
 {% macro drop_target_schema_if_exists() %}
 
-	{{ log('Preparing to drop schema: ' ~ schema_to_drop , True) }}
+	{{ log('Preparing to drop schema: ' ~ target.schema , True) }}
 
 	{# replace the database below with your database name #}
 	{% set drop_statement = 'drop schema if exists development.' ~ target.schema %}
